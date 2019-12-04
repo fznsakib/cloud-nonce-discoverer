2050783d13e340fc44c93b8bc253a332ef1b5a2d
fznsakib <fznsakib@gmail.com>
2019-12-04 12:18:17 +0000

    Added script to automate client script

---
+++ executer.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+# Start taking logs from 19:00:00 3/12/2019
+
+difficulties = [1, 2, 4, 8, 16, 24, 28]
+repeats = 2
+
+# for difficulty in difficulties:
+for instance in range(1, 13):
+    for i in range(0, repeats):
+        os.system(f'python client.py -d {24} -i {instance}')
+        time.sleep(5)
