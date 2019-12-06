97568cab6ff99928eda7a9cc27a75ec6cbedec77
fznsakib <fznsakib@gmail.com>
2019-12-06 10:05:09 +0000

    Update on_input.py

--- lambda/on_input.py
+++ lambda/on_input.py
@@ -10,29 +10,31 @@ def lambda_handler(event, context):
     message_body = json.loads(event['Records'][0]['body'])
     
     # Get instance_id to run script on
-    instance_id = message_body['instance_id']
+    instance_id = message_body['instanceId']
 
-    # Get difficulty to use as parameter for script
+    # Get parameters to run for pow.py
     difficulty = message_body['difficulty']
+    start_nonce = message_body['startNonce']
+    end_nonce = message_body['endNonce']
+    date_time = message_body['dateTime']
+    log_on_scram = message_body['logOnScram']
+    bucket_name = message_body['bucketName']
 
-    command_download_script = 'aws s3 cp s3://faizaanbucket/cnd.py home/ec2-user/cnd.py'
-    command_run_script = f'sudo python3 home/ec2-user/cnd.py --d {difficulty}'
+    command_download_script = f'aws s3 cp s3://{bucket_name}/pow.py home/ec2-user/pow.py'
+    command_run_script = f'sudo python3 home/ec2-user/pow.py --start {start_nonce} --end {end_nonce} --difficulty {difficulty} --id {instance_id} --datetime {date_time}'
     
     commands = [command_download_script, command_run_script]
 
-    # Check if instance exists
-    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    instance_ids = []
-    for instance in instances:
-        instance_ids.append(instance.id)
-
-    if (instance_id not in instance_ids):
-        return {
-            'statusCode': 404
+    # Send command to download and then execute script on instance
+    ssmresponse = ssm.send_command(
+        InstanceIds=[instance_id], 
+        DocumentName='AWS-RunShellScript',
+        Parameters= { 'commands': commands },
+        CloudWatchOutputConfig={
+            'CloudWatchLogGroupName': 'run_command_test',
+            'CloudWatchOutputEnabled': True
         }
-    
-    # Send command to download script and execute to instance
-    ssmresponse = ssm.send_command(InstanceIds=[instance_id], DocumentName='AWS-RunShellScript', Parameters= { 'commands': commands } ) 
+    )
     
     return {
         'statusCode': 200
